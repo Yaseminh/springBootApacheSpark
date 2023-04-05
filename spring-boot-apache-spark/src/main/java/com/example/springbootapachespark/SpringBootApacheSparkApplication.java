86f1bb37efbe009f36d1bfb5d93da2515cc47353
Yasemin solmaz <35719695+Yaseminh@users.noreply.github.com>
2023-04-05 14:30:53 +0300

    first commit

---
+++ spring-boot-apache-spark/src/main/java/com/example/springbootapachespark/SpringBootApacheSparkApplication.java
@@ -0,0 +1,16 @@
+package com.example.springbootapachespark;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class SpringBootApacheSparkApplication {
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootApacheSparkApplication.class, args);
+	}
+
+}
---
+++ spring-boot-apache-spark/src/test/java/com/example/springbootapachespark/SpringBootApacheSparkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootapachespark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootApacheSparkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
